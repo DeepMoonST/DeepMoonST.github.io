a8ed1c52567591de8f72af00143feae808d11bb6
DeepMoonST <wsstybw@gmail.com>
2025-05-15 19:56:16 +0300

    Delete register.php

--- register.php
+++
@@ -1,9 +0,0 @@
-<?php
-$conn = new mysqli("localhost", "root", "", "deepmoon");
-
-$username = $_POST['username'];
-$password = password_hash($_POST['password'], PASSWORD_BCRYPT);
-
-$conn->query("INSERT INTO users (username, password) VALUES ('$username', '$password')");
-echo "Kayıt başarılı!";
-?>
